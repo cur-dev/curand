bab07e3a5da7ff4a8b1308e6b2664396d801a4df
wrathematics <wrathematics@gmail.com>
2018-10-16 06:56:15 -0400

    add common header

---
+++ src/Rcurand.h
@@ -0,0 +1,22 @@
+#ifndef __RCURAND__
+#define __RCURAND__
+
+
+#include <float/float32.h>
+
+#include <R.h>
+#include <Rinternals.h>
+
+#define MAX(a,b) ((a)<(b)?(b):(a))
+#define MIN(a,b) ((a)<(b)?(a):(b))
+
+#define INT(x) INTEGER(x)[0]
+#define DBL(x) REAL(x)[0]
+
+#define RCURAND_ERROR -1
+
+#define TYPE_DOUBLE 0
+#define TYPE_FLOAT 1
+
+
+#endif
