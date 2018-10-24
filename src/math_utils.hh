e91f4c059b59cb229a8c9c47f7099017d17c82cc
wrathematics <wrathematics@gmail.com>
2018-10-24 07:20:44 -0400

    add some double/float math wrappers

---
+++ src/math_utils.hh
@@ -0,0 +1,26 @@
+#ifndef _MATH_UTILS_H_
+#define _MATH_UTILS_H_
+
+
+__device__ static inline float Pow(const float x, const float y)
+{
+  return powf(x, y);
+}
+
+__device__ static inline double Pow(const double x, const double y)
+{
+  return pow(x, y);
+}
+
+__device__ static inline float Log(const float x)
+{
+  return log(x);
+}
+
+__device__ static inline double Log(const double x)
+{
+  return log(x);
+}
+
+
+#endif
